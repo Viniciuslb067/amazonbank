2ae13e286a2c8448eba5963d9c4928299e8ee229
Viniciuslb <viniciuslb067@gmail.com>
2021-05-29 19:35:32 -0300

    feat: header component

---
+++ src/components/header/index.js
@@ -0,0 +1,51 @@
+import { GiTreeBranch } from "react-icons/gi";
+import { CgEnter } from "react-icons/cg";
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <>
+      <div className={styles.headerWrapper}>
+        <header className={styles.headerContainer}>
+          <div className={styles.header}>
+            <div className={styles.content}>
+              <div className={styles.brand}>
+                <GiTreeBranch size={40} className={styles.brandLogo} />
+              </div>
+              <nav className={styles.list}>
+                <li>
+                  <a>Página inicial</a>
+                </li>
+
+                <li>
+                  <a>Para você</a>
+                </li>
+
+                <li>
+                  <a>Conta PJ</a>
+                </li>
+
+                <li>
+                  <a>O Amazon Bank</a>
+                </li>
+
+                <li>
+                  <a>Perguntas</a>
+                </li>
+              </nav>
+              <div className={styles.login}>
+                <div>
+                  <div>
+                    <a href="#">Login</a>
+                    <CgEnter size={35} className={styles.iconLogin} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
