18d19ca61529286f25a98bc52ad8a52f185955ea
Viniciuslb <viniciuslb067@gmail.com>
2021-05-29 19:35:56 -0300

    feat: including component

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import "../styles/globals.scss";
+
+import { Header } from "../components/header";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
